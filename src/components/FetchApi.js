8528f76a119df676284412657ceb35a1f9956417
NalaElRayes <54401398+NalaElRayes@users.noreply.github.com>
2021-04-15 10:34:36 +0200

    tty rendering list

--- src/components/FetchApi.js
+++ src/components/FetchApi.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react'
 
 function FetchAPI (){
 
+const [data, setData] = useState([]);
+    
 const apiGet = () => {
 fetch('./test.json')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then((json) => {
+    console.log(json);
+    setData(json);
+  });
 
-}
+};
+
+useEffect(() =>{
+    apiGet();
+  }, []);
 
 
 
     return (
             <div>
-                My API <br/>
+                My API <br />
 
-                <button onClick={apiGet}>Fetch API</button>
+            <button onClick={apiGet}>Fetch API</button>
+            <br /> 
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+
+            <div>
+                <ul>
+                    {data.map(item => 
+                    <li>{item.title}</li>
+                    )}
+                </ul>
+            </div>
+            
+            
+
             </div>
         )
 
