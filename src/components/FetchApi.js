51a4649127ec7457e108eb0449ed142e764e8357
NalaElRayes <54401398+NalaElRayes@users.noreply.github.com>
2021-04-19 13:28:37 +0200

    creat table and search functionality
    Creating a table in material ui and rendering the logs.

--- src/components/FetchApi.js
+++ src/components/FetchApi.js
@@ -1,7 +1,43 @@
 import React, {useEffect, useState}from 'react'
 
+//Material Ui table import
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+//Table Ui
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 700,
+    },
+  });
+
 function FetchAPI (){
 
+const classes = useStyles();
 const [data, setData] = useState([]);
 const [search, setSearch] = useState(""); 
     
@@ -15,6 +51,7 @@ fetch('./test.json')
 
 };
 
+
 useEffect(() =>{
     apiGet();
   }, []);
@@ -25,7 +62,7 @@ useEffect(() =>{
              <div>
             {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
 
-            <div>
+           
                 <h4>lets the first user write a message in the first conversation</h4>
                  
                  {/*Search field*/}
@@ -34,22 +71,46 @@ useEffect(() =>{
                  }}>
                  </input>
 
+            
+
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="customized table">
+                            <TableHead>
+                            <TableRow>
+                                <StyledTableCell>Type</StyledTableCell>
+                                <StyledTableCell align="right">Severity</StyledTableCell>
+                                <StyledTableCell align="right">Message</StyledTableCell>
+                                
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {data
+                                .filter((item)=>{
+                                    if (search == ""){
+                                        return item;
+                                    }else if (item.message.toLowerCase().includes(search.toLowerCase())){
+                                        return item;
+                                    }
+                                })
+                                .map((item)  =>{
+                                    return (
+                                        <StyledTableRow>
+                                        <StyledTableCell component="th" scope="row">
+                                            {item.type}
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right">{item.severity}</StyledTableCell>
+                                        <StyledTableCell align="right">{item.message}</StyledTableCell>
+                                        </StyledTableRow>
+                                    ); 
+                                    })}
+                                
+                         
+                            </TableBody>
+                        </Table>
+                        </TableContainer>
+
                 
-                    {data
-                    .filter((item)=>{
-                        if (search == ""){
-                            return item;
-                        }else if (item.message.toLowerCase().includes(search.toLowerCase())){
-                            return item;
-                        }
-                    })
-                    .map((item =>
-                    <p>Type: {item.type} Severity: {item.severity} Message: {item.message}</p>
-                        
-                        
-                    ))}
-                
-            </div>
+           
          </div> 
         );
 
